6e4d30c2ccedbbe3db39b2efea7665e1fe12160e
Kayleigh Watts <kwatts949@gmail.com>
2022-12-13 12:19:38 +0000

    Add context to refresh page automatically

---
+++ frontend/src/components/WorkoutForm.js
@@ -0,0 +1,72 @@
+import { useEffect } from "react";
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+
+const { useState } = require("react");
+
+const WorkoutForm = () => {
+  const {dispatch} = useWorkoutsContext()
+  const [title, setTitle] = useState(""); // sets states which update when user types something
+  const [load, setLoad] = useState("");
+  const [reps, setReps] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // stops page from refreshing straight away
+
+    const workout = { title, load, reps };
+
+    const response = await fetch("/api/workouts", {
+      method: "POST",
+      body: JSON.stringify(workout),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const json = await response.json();
+
+    if (!response.ok) {
+      setError(json.error);
+    }
+
+    if (response.ok) {
+      setTitle("");
+      setLoad("");
+      setReps(""); // reset form
+      setError(null);
+      console.log("Workout added", json);
+      dispatch({type: 'CREATE_WORKOUT', payload: json })
+    }
+  };
+
+  return (
+    <form className="create" onSubmit={handleSubmit}>
+      <h3>Add a new workout</h3>
+
+      <label>Exercise title:</label>
+      <input
+        type="text"
+        onChange={(e) => setTitle(e.target.value)} //use the value to change the state of setTitle
+        value={title}
+      />
+
+      <label>Exercise load (kg):</label>
+      <input
+        type="text"
+        onChange={(e) => setLoad(e.target.value)}
+        value={load}
+      />
+
+      <label>Exercise reps:</label>
+      <input
+        type="text"
+        onChange={(e) => setReps(e.target.value)}
+        value={reps}
+      />
+
+      <button>Add workout</button>
+      {error && <div className="error">{error}</div>}
+      </form>
+  );
+};
+
+export default WorkoutForm;
